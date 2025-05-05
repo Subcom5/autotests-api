123031eb172c32667367314298778f6b34822352
Artem <shipunovartem85@gmail.com>
2025-05-05 12:31:02 +0300

    Применяем Pydantic в API клиентах

--- clients/courses/courses_client.py
+++ clients/courses/courses_client.py
@@ -1,9 +1,9 @@
 from typing import TypedDict
 from httpx import Response
 from clients.api_client import APIClient
-from clients.files.files_client import File
-from clients.private_http_builder import AuthenticationUserDict, get_private_http_client
-from clients.users.private_users_client import User
+from clients.files.files_schema import FileSchema
+from clients.private_http_builder import AuthenticationUserSchema, get_private_http_client
+from clients.users.users_schema import UserSchema
 
 
 class Course(TypedDict):
@@ -15,9 +15,9 @@ class Course(TypedDict):
     maxScore: int
     minScore: int
     description: str
-    previewFile: File  # Вложенная структура пользователя
+    previewFile: FileSchema  # Вложенная структура пользователя
     estimatedTime: str
-    createdByUser: User  # Вложенная структура файла
+    createdByUser: UserSchema  # Вложенная структура файла
 
 
 class GetCursesQueryDict(TypedDict):
@@ -110,7 +110,7 @@ class CoursesClient(APIClient):
         return response.json()
 
 # Добавляем builder для CoursesClient
-def get_courses_client(user: AuthenticationUserDict) -> CoursesClient:
+def get_courses_client(user: AuthenticationUserSchema) -> CoursesClient:
     """
     Функция создаёт экземпляр CoursesClient с уже настроенным HTTP-клиентом.
 
--- clients/exercises/exercises_client.py
+++ clients/exercises/exercises_client.py
@@ -1,5 +1,5 @@
 from clients.api_client import APIClient
-from clients.private_http_builder import AuthenticationUserDict, get_private_http_client
+from clients.private_http_builder import AuthenticationUserSchema, get_private_http_client
 from httpx import Response
 from typing import TypedDict
 
@@ -148,7 +148,7 @@ class ExercisesClient(APIClient):
         return response.json()
 
 
-def get_exercise_client(user: AuthenticationUserDict) -> ExercisesClient:
+def get_exercise_client(user: AuthenticationUserSchema) -> ExercisesClient:
     """
     Функция создаёт экземпляр ExercisesClient с уже настроенным HTTP-клиентом.
 
