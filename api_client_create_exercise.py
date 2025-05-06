1b4a6c9a103cbe383d1c42823878985eb939af52
Artem <shipunovartem85@gmail.com>
2025-05-06 15:59:42 +0300

    Знакомство с Faker. Практика работы с генерацией случайных данных.

--- api_client_create_exercise.py
+++ api_client_create_exercise.py
@@ -2,22 +2,16 @@ from clients.courses.courses_client import get_courses_client
 from clients.courses.courses_schema import CreateCourseRequestSchema
 from clients.exercises.exercises_client import get_exercise_client
 from clients.exercises.exercises_schema import CreateExerciseRequestSchema, GetExercisesQuerySchema, \
-    UpdateExerciseRequestSchema, GetExerciseResponseSchema
+    UpdateExerciseRequestSchema
 from clients.files.files_client import get_files_client, CreateFileRequestSchema
 from clients.private_http_builder import AuthenticationUserSchema
 from clients.users.public_users_client import get_public_users_client, CreateUserRequestSchema
-from tools.fakers import fake
+
 
 public_users_client = get_public_users_client()
 
 # Создаем пользователя
-create_user_request = CreateUserRequestSchema(
-    email=fake.email(),
-    password="string",
-    lastName="string",
-    firstName="string",
-    middleName="string"
-)
+create_user_request = CreateUserRequestSchema()
 create_user_response = public_users_client.create_user(create_user_request)
 
 # Инициализируем клиенты
@@ -30,21 +24,12 @@ courses_client = get_courses_client(authentication_user)
 exercise_client = get_exercise_client(authentication_user)
 
 # Загружаем файл
-create_file_request = CreateFileRequestSchema(
-    filename="server_image.png",
-    directory="courses",
-    upload_file="./testdata/files/server_image.png"
-)
+create_file_request = CreateFileRequestSchema(upload_file="./testdata/files/server_image.png")
 create_file_response = files_client.create_file(create_file_request)
 print('Create file data:', create_file_response)
 
 # Создаем курс
 create_course_request = CreateCourseRequestSchema(
-    title="Python",
-    maxScore=100,
-    minScore=10,
-    description="Python API course",
-    estimatedTime="2 weeks",
     previewFileId=create_file_response.file.id,
     createdByUserId=create_user_response.user.id
 )
@@ -52,15 +37,7 @@ create_course_response = courses_client.create_course(create_course_request)
 print('Create course data:', create_course_response)
 
 # Создаем упражнение
-create_exercise_request = CreateExerciseRequestSchema(
-    title="Exercise 1",
-    courseId=create_course_response.course.id,
-    maxScore= 5,
-    minScore= 1,
-    orderIndex= 0,
-    description="Упражнение 1",
-    estimatedTime="100 минут"
-)
+create_exercise_request = CreateExerciseRequestSchema(courseId=create_course_response.course.id,)
 create_exercise_response = exercise_client.create_exercise(create_exercise_request)
 print('Create exercise data:', create_exercise_response)
 
@@ -68,15 +45,7 @@ print('Create exercise data:', create_exercise_response)
 # Проверка остальных методов.
 
 # Создаем упражнение
-create_exercise_request = CreateExerciseRequestSchema(
-    title="Exercise 2",
-    courseId=create_course_response.course.id,
-    maxScore= 5,
-    minScore= 1,
-    orderIndex= 0,
-    description="Упражнение 2",
-    estimatedTime="90 минут"
-)
+create_exercise_request = CreateExerciseRequestSchema(courseId=create_course_response.course.id,)
 create_exercise_response = exercise_client.create_exercise(create_exercise_request)
 print('Create exercise data:', create_exercise_response)
 
@@ -92,17 +61,9 @@ get_exercises_response = exercise_client.get_exercises(
 )
 print('Get list exercise data:', get_exercises_response)
 
-
 # Обновляем упражнение "Упражнение 1".
 update_exercise_response = exercise_client.update_exercise(
     create_exercise_response.exercise.id,
-    UpdateExerciseRequestSchema(
-        title="Exercise 2",
-        maxScore=100,
-        minScore=50,
-        orderIndex=0,
-        description="Updated description 2",
-        estimatedTime="2 часа"
-    )
+    UpdateExerciseRequestSchema()
 )
 print("Updated exercise:", update_exercise_response)
