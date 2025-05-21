7607701d33cf8a94e8b2bf2bd0c4efe72fef1473
Artem <shipunovartem85@gmail.com>
2025-05-21 11:10:09 +0300

    Практикуемся в написании API автотестов. Часть 3. Практика написания API автотестов для работы с заданиями. Удаление задания.

--- tests/exercises/test_exercises.py
+++ tests/exercises/test_exercises.py
@@ -2,6 +2,7 @@ import pytest
 
 from http import HTTPStatus
 
+from clients.errors_schema import InternalErrorResponseSchema
 from clients.exercises.exercises_client import ExercisesClient
 from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema, \
     GetExerciseResponseSchema, UpdateExerciseResponseSchema, UpdateExerciseRequestSchema
@@ -9,7 +10,7 @@ from fixtures.courses import CourseFixture
 from fixtures.exercises import ExerciseFixture
 from tools.assertions.base import assert_status_code
 from tools.assertions.exercises import assert_create_exercise_response, assert_get_exercise_response, \
-    assert_update_exercise_response
+    assert_update_exercise_response, assert_exercise_not_found_response
 from tools.assertions.schema import validate_json_schema
 
 
@@ -59,3 +60,19 @@ class TestExercise:
 
         validate_json_schema(response.json(), response_data.model_json_schema())
 
+    def test_delete_exercise(
+            self,
+            exercise_client: ExercisesClient,
+            function_exercise: ExerciseFixture
+    ):
+        delete_response = exercise_client.delete_exercise_api(function_exercise.response.exercise.id)
+
+        assert_status_code(delete_response.status_code, HTTPStatus.OK)
+
+        get_response = exercise_client.get_exercise_api(exercise_id=function_exercise.response.exercise.id)
+        get_response_data = InternalErrorResponseSchema.model_validate_json(get_response.text)
+
+        assert_status_code(get_response.status_code, HTTPStatus.NOT_FOUND)
+        assert_exercise_not_found_response(get_response_data)
+
+        validate_json_schema(get_response.json(), get_response_data.model_json_schema())
