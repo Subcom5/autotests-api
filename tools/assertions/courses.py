e8aa68fad84f64439b0da56dcb841f3392bca6d2
Artem <shipunovartem85@gmail.com>
2025-05-21 10:26:56 +0300

    Поправил assert_update_course_response.

--- tools/assertions/courses.py
+++ tools/assertions/courses.py
@@ -18,7 +18,7 @@ def assert_update_course_response(
     :raises AssertionError: Если хотя бы одно поле не совпадает.
     """
     assert_equal(response.course.title, request.title, "title")
-    assert_equal(response.course.max_sore, request.max_sore, "max_sore")
+    assert_equal(response.course.max_score, request.max_score, "max_score")
     assert_equal(response.course.min_score, request.min_score, "min_score")
     assert_equal(response.course.description, request.description, "description")
     assert_equal(response.course.estimated_time, request.estimated_time, "estimated_time")
