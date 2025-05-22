fe393e45c065576466cbe57087e663db323bc300
Artem <shipunovartem85@gmail.com>
2025-05-22 11:37:26 +0300

    Интеграция Allure в автотесты. Практическая работа с Allure аннотациями.

--- tests/authentication/test_authentication.py
+++ tests/authentication/test_authentication.py
@@ -8,6 +8,7 @@ from clients.authentication.authentication_schema import LoginResponseSchema
 from clients.private_http_builder import AuthenticationUserSchema
 from fixtures.users import UserFixture
 from tools.allure.epics import AllureEpic
+from tools.allure.features import AllureFeature
 from tools.allure.stories import AllureStory
 from tools.allure.tags import AllureTag
 from tools.assertions.authentication import assert_login_response
@@ -19,10 +20,14 @@ from tools.assertions.schema import validate_json_schema
 @pytest.mark.authentication
 @allure.tag(AllureTag.REGRESSION, AllureTag.AUTHENTICATION)
 @allure.epic(AllureEpic.LMS)
+@allure.feature(AllureFeature.AUTHENTICATION)
+@allure.parent_suite(AllureEpic.LMS)
+@allure.suite(AllureFeature.AUTHENTICATION)
 class TestAuthentication:
     @allure.story(AllureStory.LOGIN)
     @allure.title("Login with correct email and password")
     @allure.severity(Severity.BLOCKER)
+    @allure.sub_suite(AllureStory.LOGIN)
     def test_login(self, function_user: UserFixture, authentication_client: AuthenticationClient):
 
         request = AuthenticationUserSchema(email=function_user.email, password=function_user.password)
--- tests/exercises/test_exercises.py
+++ tests/exercises/test_exercises.py
@@ -27,11 +27,14 @@ from tools.assertions.schema import validate_json_schema
 @allure.tag(AllureTag.EXERCISES, AllureTag.REGRESSION)
 @allure.epic(AllureEpic.LMS)
 @allure.feature(AllureFeature.EXERCISES)
+@allure.parent_suite(AllureEpic.LMS)
+@allure.suite(AllureFeature.EXERCISES)
 class TestExercise:
     @allure.tag(AllureTag.CREATE_ENTITY)
     @allure.story(AllureStory.CREATE_ENTITY)
     @allure.title("Create exercise")
     @allure.severity(Severity.BLOCKER)
+    @allure.sub_suite(AllureStory.CREATE_ENTITY)
     def test_create_exercise(
             self,
             exercise_client: ExercisesClient,
@@ -52,6 +55,7 @@ class TestExercise:
     @allure.story(AllureStory.GET_ENTITY)
     @allure.title("Get exercise")
     @allure.severity(Severity.BLOCKER)
+    @allure.sub_suite(AllureStory.GET_ENTITY)
     def test_get_exercise(
             self,
             exercise_client: ExercisesClient,
@@ -69,6 +73,7 @@ class TestExercise:
     @allure.story(AllureStory.UPDATE_ENTITY)
     @allure.title("Update exercise")
     @allure.severity(Severity.CRITICAL)
+    @allure.sub_suite(AllureStory.UPDATE_ENTITY)
     def test_update_exercise(
             self,
             exercise_client: ExercisesClient,
@@ -87,6 +92,7 @@ class TestExercise:
     @allure.story(AllureStory.DELETE_ENTITY)
     @allure.title("Delete exercise")
     @allure.severity(Severity.CRITICAL)
+    @allure.sub_suite(AllureStory.DELETE_ENTITY)
     def test_delete_exercise(
             self,
             exercise_client: ExercisesClient,
@@ -108,6 +114,7 @@ class TestExercise:
     @allure.story(AllureStory.GET_ENTITIES)
     @allure.title("Get exercises")
     @allure.severity(Severity.BLOCKER)
+    @allure.sub_suite(AllureStory.GET_ENTITIES)
     def test_get_exercises(
             self,
             exercise_client: ExercisesClient,
